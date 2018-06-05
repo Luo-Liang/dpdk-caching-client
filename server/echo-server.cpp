51f504c1c783b8d88580d9f0b58e1e20be357e00
Ubuntu <ubuntu@ip-172-31-13-4.us-west-2.compute.internal>
2018-06-05 04:53:14 +0000

    c++-ify

--- server/echo-server.cpp
+++ server/echo-server.cpp
@@ -58,16 +58,18 @@
 #define BATCH_SIZE 32
 //#define PERF_DEBUG
 
-struct lcore_args {
+struct lcore_args
+{
     int *src_id, des_id;
     enum pkt_type type;
     uint8_t tid;
     //volatile enum benchmark_phase *phase;
     struct rte_mempool *pool;
-    char* ifid;
+    char *ifid;
 } __attribute__((packed));
 
-struct settings{
+struct settings
+{
     uint32_t warmup_time;
     uint32_t run_time;
     uint32_t cooldown_time;
@@ -91,7 +93,7 @@ struct settings{
 };*/
 
 static inline int
-port_init(struct lcore_args *largs, 
+port_init(struct lcore_args *largs,
           uint8_t threadnum)
 {
     rte_eth_conf port_conf_default;
@@ -100,13 +102,13 @@ port_init(struct lcore_args *largs,
     port_conf_default.rx_adv_conf.rss_conf.rss_hf = ETH_RSS_NONFRAG_IPV4_UDP;
     port_conf_default.fdir_conf.mode = RTE_FDIR_MODE_NONE;
     struct rte_eth_conf port_conf = port_conf_default;
-    uint8_t q, rx_rings, tx_rings, numports;
+    uint8_t q, rx_rings, tx_rings;
     int retval, i;
     char bufpool_name[32];
     struct ether_addr myaddr;
     uint16_t port = 65535;
 
-    numports = rte_eth_dev_count_avail();
+    /*numports = rte_eth_dev_count_avail();
     printf("Number of ports of the server is %"PRIu8 "\n", numports);
 
     for(i = 0; i < numports; i++)
@@ -124,9 +126,10 @@ port_init(struct lcore_args *largs,
 
     if(port == -1)
     {
-        
+        printf("cannot find requested device %s\n", largs->ifid);
+        exit(-1);
     }
-
+    numports
     for (i = 0; i < threadnum; i++) {
         largs[i].tid = i;
         sprintf(bufpool_name, "bufpool_%d", i);
@@ -141,38 +144,67 @@ port_init(struct lcore_args *largs,
             rte_eth_macaddr_get(port, &myaddr);
             largs[i].src_id[port] = get_endhost_id(myaddr);
         }
+    }*/
+
+    int nb_ports = rte_eth_dev_count_avail();
+    printf("Number of ports of the server is %" PRIu8 "\n", nb_ports);
+
+    for (i = 0; i < threadnum; i++)
+    {
+        largs[i].tid = i;
+        sprintf(bufpool_name, "bufpool_%d", i);
+        largs[i].pool = rte_pktmbuf_pool_create(bufpool_name,
+                                                NUM_MBUFS * threadnum, MBUF_CACHE_SIZE, 0,
+                                                RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+        if (largs[i].pool == NULL)
+        {
+            rte_exit(EXIT_FAILURE, "Error: rte_pktmbuf_pool_create failed\n");
+        }
+        largs[i].src_id = (int *)malloc(sizeof(int) * nb_ports);
+        for (port = 0; port < nb_ports; port++)
+        {
+            rte_eth_macaddr_get(port, &myaddr);
+            largs[i].src_id[port] = get_endhost_id(myaddr);
+        }
     }
 
-    for (port = 0; port < nb_ports; port++) {
+    for (port = 0; port < nb_ports; port++)
+    {
         rx_rings = tx_rings = threadnum;
 
         /* Configure the Ethernet device of a given port */
         retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
-        if (retval != 0) {
+        if (retval != 0)
+        {
             return retval;
         }
 
         /* Allocate and set up RX queues for a given Ethernet port */
-        for (q = 0; q < rx_rings; q++) {
+        for (q = 0; q < rx_rings; q++)
+        {
             retval = rte_eth_rx_queue_setup(port, q, RX_RING_SIZE,
-                    rte_eth_dev_socket_id(port), NULL, largs[q].pool);
-            if (retval < 0) {
+                                            rte_eth_dev_socket_id(port), NULL, largs[q].pool);
+            if (retval < 0)
+            {
                 return retval;
             }
         }
 
         /* Allocate and set up TX queues for a given Ethernet port */
-        for (q = 0; q < tx_rings; q++) {
+        for (q = 0; q < tx_rings; q++)
+        {
             retval = rte_eth_tx_queue_setup(port, q, TX_RING_SIZE,
-                    rte_eth_dev_socket_id(port), NULL);
-            if (retval < 0) {
+                                            rte_eth_dev_socket_id(port), NULL);
+            if (retval < 0)
+            {
                 return retval;
             }
         }
 
         /* Start the Ethernet port */
         retval = rte_eth_dev_start(port);
-        if (retval < 0) {
+        if (retval < 0)
+        {
             return retval;
         }
 
@@ -205,43 +237,50 @@ lcore_execute(__attribute__((unused)) void *arg)
     bsz = BATCH_SIZE;
     nb_ports = rte_eth_dev_count_avail();
 
-    printf("Server worker %"PRIu8 " started\n", myarg->tid);
+    printf("Server worker %" PRIu8 " started\n", myarg->tid);
 
-    do {
+    do
+    {
 #ifdef PERF_DEBUG
         gettimeofday(&start, NULL);
-#endif 
+#endif
 
-        for (port = 0; port < nb_ports; port++) {
+        for (port = 0; port < nb_ports; port++)
+        {
             /* Receive and process requests */
-            if ((n = rte_eth_rx_burst(port, queue, bufs, bsz)) < 0) {
+            if ((n = rte_eth_rx_burst(port, queue, bufs, bsz)) < 0)
+            {
                 rte_exit(EXIT_FAILURE, "Error: rte_eth_rx_burst failed\n");
             }
 #ifdef PERF_DEBUG
             gettimeofday(&end, NULL);
             recv_elapsed += ((end.tv_sec * 1000000 + end.tv_usec) -
-                    (start.tv_sec * 1000000 + start.tv_usec));
-            if ((!(recv_cnt % 1000000)) && (recv_cnt != 0) 
-                    && (recv_cnt < 10000000)) {
-                printf("recv time %lf\n", 
-                        (recv_elapsed + 0.0)/(recv_cnt + 0.0));
+                             (start.tv_sec * 1000000 + start.tv_usec));
+            if ((!(recv_cnt % 1000000)) && (recv_cnt != 0) && (recv_cnt < 10000000))
+            {
+                printf("recv time %lf\n",
+                       (recv_elapsed + 0.0) / (recv_cnt + 0.0));
             }
 #endif
 
-            for (i = 0; i < n; i++) {
+            for (i = 0; i < n; i++)
+            {
                 drops[i] = pkt_server_process(bufs[i], myarg->type);
             }
 
-            for (i = 0, j = 0; i < n; i++) {
-                if (drops[i]) {
+            for (i = 0, j = 0; i < n; i++)
+            {
+                if (drops[i])
+                {
                     rte_pktmbuf_free(bufs[i]);
-                } else {
+                }
+                else
+                {
                     response[j++] = bufs[i];
 
 #ifdef PERF_DEBUG
                     recv_cnt++;
 #endif
-
                 }
             }
 
@@ -250,7 +289,8 @@ lcore_execute(__attribute__((unused)) void *arg)
 #endif
 
             i = 0;
-            while (i < j) {
+            while (i < j)
+            {
                 n = rte_eth_tx_burst(port, queue, response + i, j - i);
                 i += n;
             }
@@ -261,35 +301,37 @@ lcore_execute(__attribute__((unused)) void *arg)
         send_elapsed += ((end.tv_sec * 1000000 + end.tv_usec) -
                          (start.tv_sec * 1000000 + start.tv_usec));
         send_cnt += j;
-        if ((!(send_cnt % 1000000)) && (send_cnt != 0) && 
-                (send_cnt < 10000000)) {
+        if ((!(send_cnt % 1000000)) && (send_cnt != 0) &&
+            (send_cnt < 10000000))
+        {
             printf("send time is %lf\n",
-                    (send_elapsed + 0.0)/(recv_cnt + 0.0));
+                   (send_elapsed + 0.0) / (recv_cnt + 0.0));
         }
 #endif
     } while (1);
 
-	return 0;
+    return 0;
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int ret, i;
-	unsigned lcore_id;
+    int ret, i;
+    unsigned lcore_id;
     uint8_t threadnum;
     struct lcore_args *largs;
 
     /* Initialize the Environment Abstraction Layer (EAL) */
-	ret = rte_eal_init(argc, argv);
-	if (ret < 0) {
+    ret = rte_eal_init(argc, argv);
+    if (ret < 0)
+    {
         rte_exit(EXIT_FAILURE, "Error: cannot init EAL\n");
     }
     argc -= ret;
     argv += ret;
 
     /* Initialize application args */
-    if (argc != 3) {
+    if (argc != 3)
+    {
         printf("Usage: %s <server type>\n", argv[0]);
         printf("Server Type:\n");
         printf("\t0 -> ECHO server\n");
@@ -300,22 +342,24 @@ main(int argc, char **argv)
 
     /* Initialize NIC ports */
     threadnum = rte_lcore_count() - 1;
-    largs = calloc(threadnum, sizeof(*largs));
-    for (i = 0; i < threadnum; i++) {
+    largs = (lcore_args*)calloc(threadnum, sizeof(*largs));
+    for (i = 0; i < threadnum; i++)
+    {
         largs[i].tid = i;
-        largs[i].type = atoi(argv[1]);
+        largs[i].type = (pkt_type)atoi(argv[1]);
     }
     port_init(largs, threadnum);
 
     /* call lcore_execute() on every slave lcore */
-    RTE_LCORE_FOREACH_SLAVE(lcore_id) {
-        rte_eal_remote_launch(lcore_execute, (void *)(largs + lcore_id -1), 
-                lcore_id);
+    RTE_LCORE_FOREACH_SLAVE(lcore_id)
+    {
+        rte_eal_remote_launch(lcore_execute, (void *)(largs + lcore_id - 1),
+                              lcore_id);
     }
 
     printf("Master core performs maintainence\n");
-	rte_eal_mp_wait_lcore();
+    rte_eal_mp_wait_lcore();
 
     free(largs);
-	return 0;
+    return 0;
 }
