1e5f352363a393e92d1265ea79a6df6cae038383
Ubuntu <ubuntu@ip-172-31-6-71.us-west-2.compute.internal>
2018-06-06 03:49:31 +0000

    + auto ip checksum and manual udp checksum

--- client/pkt-utils.cpp
+++ client/pkt-utils.cpp
@@ -58,6 +58,46 @@ ip_2_uint32(uint8_t ip[])
     return myip;
 }
 
+uint16_t udp_checksum(udphdr *p_udp_header, /*size_t len,*/ uint32_t src_addr, uint32_t dest_addr)
+{
+  uint16_t *buf = (uint16_t*)p_udp_header;
+  uint16_t *ip_src = (uint16_t*)&src_addr, *ip_dst = (uint16_t*)&dest_addr;
+  uint32_t sum;
+  size_t length = p_udp_header->uh_ulen;
+  size_t len = length;
+  // Calculate the sum
+  sum = 0;
+  while (len > 1)
+    {
+      sum += *buf++;
+      if (sum & 0x80000000)
+	sum = (sum & 0xFFFF) + (sum >> 16);
+      len -= 2;
+    }
+
+  if (len & 1)
+    // Add the padding if the packet lenght is odd
+    sum += *((uint8_t*)buf);
+
+  // Add the pseudo-header
+  sum += *(ip_src++);
+  sum += *ip_src;
+
+  sum += *(ip_dst++);
+  sum += *ip_dst;
+
+  sum += htons(IPPROTO_UDP);
+  sum += htons(length);
+
+  // Add the carries
+  while (sum >> 16)
+    sum = (sum & 0xFFFF) + (sum >> 16);
+
+  // Return the one's complement of sum
+  return (uint16_t)~sum;
+}
+
+
 void
 pkt_header_build(char *pkt_ptr,
                  int src_id,
@@ -73,7 +113,7 @@ pkt_header_build(char *pkt_ptr,
     rte_memcpy(myhdr->ether.d_addr.addr_bytes, mydes->mac, ETHER_ADDR_LEN);
     rte_memcpy(myhdr->ether.s_addr.addr_bytes, mysrc->mac, ETHER_ADDR_LEN);
     myhdr->ether.ether_type = htons(ETHER_TYPE_IPv4);
-
+    udphdr uhdr;
     // IP header
     myhdr->ip.version_ihl = 4 << 4 | 5;
     myhdr->ip.total_length = pkt_size(type) - ETHER_HEADER_LEN;
@@ -82,21 +122,23 @@ pkt_header_build(char *pkt_ptr,
     myhdr->ip.time_to_live = 64;
     myhdr->ip.next_proto_id = 17;
     myhdr->ip.hdr_checksum = 0;
+    myhdr->ip.hdr_checksum = rte_ipv4_cksum(&myhdr->ip);
     myhdr->ip.src_addr = ip_2_uint32(mysrc->ip);
     myhdr->ip.dst_addr = ip_2_uint32(mydes->ip);
     //printf("building a udp packet from ip = %d.%d.%d.%d to %d.%d.%d.%d\n", mysrc->ip[0], mysrc->ip[1], mysrc->ip[2], mysrc->ip[3], mydes->ip[0], mydes->ip[1], mydes->ip[2], mydes->ip[3]); 
     // UDP header
-    myhdr->udp.src_port = UDP_SRC_PORT + tid;
-    myhdr->udp.dst_port = UDP_DES_PORT;
-    myhdr->udp.dgram_len = pkt_size(type) - ETHER_HEADER_LEN - IP_HEADER_LEN - 
+    myhdr->udp.src_port = uhdr.uh_sport = UDP_SRC_PORT + tid;
+    myhdr->udp.dst_port = uhdr.uh_dport = UDP_DES_PORT;
+    myhdr->udp.dgram_len = uhdr.uh_ulen = pkt_size(type) - ETHER_HEADER_LEN - IP_HEADER_LEN - 
         UDP_HEADER_LEN;
-    myhdr->udp.dgram_cksum = 0;
+    myhdr->udp.dgram_cksum = uhdr.uh_sum = 0;
+    myhdr->udp.dgram_cksum = udp_checksum(&uhdr, myhdr->ip.src_addr, myhdr->ip.dst_addr);
 }
 
 void
 pkt_set_attribute(struct rte_mbuf *buf)
 {
-    buf->ol_flags |= PKT_TX_IP_CKSUM | PKT_TX_IPV4;
+    buf->ol_flags |=  PKT_TX_IPV4;// | PKT_TX_UDP_CKSUM ;//| PKT_TX_UDP_CKSUM PKT_TX_IP_CKSUM |
     buf->l2_len = sizeof(struct ether_hdr);
     buf->l3_len = sizeof(struct ipv4_hdr);
 }
--- client/pkt-utils.h
+++ client/pkt-utils.h
@@ -11,11 +11,19 @@ enum pkt_type {
     ECHO,
 };
 
+struct udphdr {
+  u_short uh_sport;/* source port */
+  u_short uh_dport;/* destination port */
+  short uh_ulen;/* udp length */
+  u_short uh_sum;/* udp checksum */
+};
+
+
 uint16_t pkt_size (enum pkt_type type);
 void pkt_header_build(char *pkt_ptr, int src_id, int des_id, 
                       enum pkt_type type, uint8_t tid);
 void pkt_set_attribute(struct rte_mbuf *buf);
 void pkt_client_data_build(char *pkt_ptr, enum pkt_type type);
 int pkt_client_process(struct rte_mbuf *buf, enum pkt_type type);
-
+uint16_t udp_checksum(udphdr*, uint32_t, uint32_t);
 #endif /* _PKT_UTILS_H */
